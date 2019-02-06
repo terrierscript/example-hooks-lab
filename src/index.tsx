aa942239c9b30659bf64c3e318f0bd3f9251200e
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-02-06 13:12:20 +0900

    bad pattern

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,38 @@
-import React from "react"
+import React, { useState, useEffect, useCallback } from "react"
 import { render } from "react-dom"
 
+const initialHelloFn = () => {
+  console.log("initial")
+}
+const useCounter = () => {
+  const [count, setCounter] = useState(0)
+  const [helloFn, setHelloFn] = useState<Function>(initialHelloFn)
+  const newFn = useCallback(() => {
+    console.log("hello!", count)
+  }, [count])
+  useEffect(() => {
+    setHelloFn(newFn)
+  }, [newFn, setHelloFn])
+  console.log(helloFn)
+  return {
+    count,
+    setCounter,
+    helloFn
+  }
+}
 export const MyApp = () => {
-  return <div>aa</div>
+  const { count, setCounter, helloFn } = useCounter()
+  return (
+    <div>
+      <div>{count}</div>
+      <div>
+        <button onClick={() => setCounter(count + 1)}>+</button>
+      </div>
+      <div>
+        <button onClick={() => helloFn()}>hello</button>
+      </div>
+    </div>
+  )
 }
 
 export const start = () => {
