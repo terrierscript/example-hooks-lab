078d0801a855a6bebb9b19afc9cf736d39921f81
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-02-06 21:14:58 +0900

    after

--- fib-example/src/diff.tsx
+++ fib-example/src/diff.tsx
@@ -1,4 +1,10 @@
-import React, { Component, ReactNode, createContext } from "react"
+import React, {
+  createContext,
+  useState,
+  useContext,
+  useEffect,
+  useCallback
+} from "react"
 
 const fib = (n) => {
   if (n < 2) {
@@ -7,85 +13,52 @@ const fib = (n) => {
   return fib(n - 1) + fib(n - 2)
 }
 
-class Fib extends Component<
-  { initialValue: number; children: (...args) => ReactNode },
-  { result: number }
-> {
-  constructor(props) {
-    super(props)
-    this.state = {
-      result: 0
-    }
-  }
-  calcFib = (n) => {
-    this.setState({ result: fib(n) })
-  }
-  componentDidMount() {
-    this.calcFib(this.props.initialValue)
-  }
-  render() {
-    return (
-      <>
-        {this.props.children({
-          result: this.state.result,
-          calcFib: this.calcFib
-        })}
-      </>
-    )
-  }
+const useFib = () => {
+  const [result, setResult] = useState(0)
+  const { count } = useContext(CounterContext)
+  const calcFib = useCallback(
+    (n) => {
+      setResult(fib(n))
+    },
+    [setResult]
+  )
+  useEffect(() => {
+    calcFib(count)
+  }, [])
+  return { result, calcFib }
 }
 
 const CounterContext = createContext({
   count: 0,
   setCount: (args) => {}
 })
-class Counter extends Component<{}, { count: number }> {
-  constructor(props) {
-    super(props)
-    this.state = {
-      count: 10
-    }
-  }
-  render() {
-    const value = {
-      count: this.state.count,
-      setCount: (cnt: number) => this.setState({ count: cnt })
-    }
-    console.log(CounterContext.Provider)
-    return (
-      <CounterContext.Provider value={value}>
-        {this.props.children}
-      </CounterContext.Provider>
-    )
-  }
+
+const Counter = ({ children }) => {
+  const [count, setCount] = useState(10)
+  const value = { count, setCount }
+  return (
+    <CounterContext.Provider value={value}>{children}</CounterContext.Provider>
+  )
 }
 
-class Main extends Component {
-  static contextType = CounterContext
-  render() {
-    const { count, setCount } = this.context
-    return (
-      <Fib initialValue={count}>
-        {({ result, calcFib }) => {
-          return (
-            <div>
-              <div>Count: {count}</div>
-              <div>
-                <button onClick={() => setCount(count + 1)}>+</button>
-              </div>
-              <div>Fib:{result}</div>
-              <div>
-                <button onClick={() => calcFib(count)}>Caclurate</button>
-              </div>
-            </div>
-          )
-        }}
-      </Fib>
-    )
-  }
+const Main = () => {
+  const { count, setCount } = useContext(CounterContext)
+  const { result, calcFib } = useFib()
+  return (
+    <div>
+      <div>Count: {count}</div>
+      <div>
+        <button onClick={() => setCount(count + 1)}>+</button>
+      </div>
+      <div>Fib:{result}</div>
+      <div>
+        <button onClick={() => calcFib(count)}>Caclurate</button>
+      </div>
+    </div>
+  )
 }
 
-export const Before = () => {
+export const After = () => {
   return (
     <Counter>
       <Main />
