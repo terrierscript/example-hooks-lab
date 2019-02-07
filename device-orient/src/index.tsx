b2895559d6e71d0915f3cf7ae4187b7ece744073
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-02-07 10:46:21 +0900

    add

--- device-orient/src/index.tsx
+++ device-orient/src/index.tsx
@@ -1,15 +1,26 @@
 import React, { useState, useEffect, useCallback } from "react"
 import { render } from "react-dom"
 import useDeviceOrientation from "@rehooks/device-orientation"
+import { useSpring, animated } from "react-spring"
+
+const trans1 = (x, y) => `translate3d(${x / 10}px,${y / 10}px,0)`
 
 function MyApp() {
   let value = useDeviceOrientation()
+  const [props, set] = useSpring(() => ({
+    xy: [value.beta, value.gamma],
+    config: { mass: 10, tension: 550, friction: 140 }
+  }))
+
   return (
     <div>
       <p>Absolute: {value.absolute}</p>
       <p>Alpha: {value.alpha}</p>
       <p>Beta: {value.beta}</p>
       <p>Gamma: {value.gamma}</p>
+      <animated.div style={{ transform: props.xy.interpolate(trans1) }}>
+        ●
+      </animated.div>
     </div>
   )
 }
