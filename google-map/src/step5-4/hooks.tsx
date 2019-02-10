464028f7191a001a1fb57d70af4b21c79901c656
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-02-10 16:13:52 +0900

    add

---
+++ google-map/src/step5-4/hooks.tsx
@@ -0,0 +1,176 @@
+import {
+  useEffect,
+  useState,
+  useCallback,
+  useReducer,
+  useRef,
+  useMemo,
+  useLayoutEffect
+} from "react"
+import GoogleMapsApiLoader from "google-maps-api-loader"
+// @ts-ignore
+import uuid from "uuid/v4"
+import ReactDOMServer from "react-dom/server"
+
+// Google Mapのオブジェクトを呼び出すだけのhooks
+export const useGoogleMap = (apiKey) => {
+  const [googleMap, setGoogleMap] = useState(null)
+  useEffect(() => {
+    GoogleMapsApiLoader({ apiKey }).then((google) => {
+      setGoogleMap(google)
+    })
+  }, []) // useEffectの第二引数を[]にすることで、初回1回目だけ実行される
+  return googleMap
+}
+
+// 実際にMapをDOMにマウントする処理。
+export const useMap = ({ googleMap, mapContainerRef, initialConfig }) => {
+  const [map, setMap] = useState(null)
+  useEffect(() => {
+    // googleMapかmapContainerRefが初期化されてなければ何もしない
+    if (!googleMap || !mapContainerRef.current) {
+      return
+    }
+    const map = new googleMap.maps.Map(mapContainerRef.current, initialConfig)
+    setMap(map)
+  }, [googleMap, mapContainerRef]) // initialConfigは変わったとしても再マウントするとおかしなことになるので更新対象にしない // googleMapかmapContainerRefが変化したらeffectが発火する。
+
+  // あとで使えるようにmapを返すようにする
+  return map
+}
+
+export const useDrawMapMarker = ({ position, googleMap, map }) => {
+  const markerObjectsRef = useRef(null)
+  useEffect(() => {
+    const { Marker } = googleMap.maps
+    // すでに描画済みなmarkerだったら描画しない
+    if (markerObjectsRef.current) {
+      return
+    }
+    const markerObj = new Marker({
+      position,
+      map,
+      title: "marker!"
+    })
+    markerObjectsRef.current = markerObj
+    // コンポーネントが消えたらmarkerもmapから消すように仕掛ける。これはすっ
+    return () => {
+      if (markerObjectsRef.current === null) {
+        return
+      }
+      markerObjectsRef.current.setMap(null)
+    }
+  }, [googleMap, map])
+  return markerObjectsRef.current
+}
+
+export const useMarkerClickEvent = (marker, onClickMarker) => {
+  // イベントが変更される事を考慮する
+  useEffect(() => {
+    if (!marker) {
+      return
+    }
+    const listener = marker.addListener("click", (e) => {
+      onClickMarker(e)
+    })
+    return () => {
+      listener.remove()
+    }
+  }, [marker, onClickMarker])
+}
+
+export const useMapInfoWindow = ({ googleMap, marker, map, contentNode }) => {
+  const infoWindow = useRef(null)
+  useEffect(() => {
+    if (!marker) {
+      return
+    }
+    if (!contentNode) {
+      return
+    }
+    // infoWindowの再描画防止
+    if (infoWindow.current) {
+      return
+    }
+    const infoWindowObj = new googleMap.maps.InfoWindow({
+      content: contentNode
+    })
+    infoWindow.current = infoWindowObj
+    marker.addListener("click", () => {
+      infoWindow.current.open(map, marker)
+    })
+  }, [googleMap, marker, contentNode])
+  return infoWindow.current
+}
+
+const markerReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+      const id = uuid()
+      return {
+        ...state,
+        [id]: action.payload
+      }
+    case "REMOVE":
+      const { [action.payload]: removeItem, ...rest } = state
+      return rest
+    default:
+      return state
+  }
+}
+
+const mapReducerInitializer = (initialMarkers) => {
+  return initialMarkers.reduce((state, marker) => {
+    return markerReducer(state, {
+      type: "ADD",
+      payload: marker
+    })
+  }, {})
+}
+
+// markerをstate管理する
+export const useMarkerState = (initialMarkers) => {
+  const [markers, dispatch] = useReducer(
+    markerReducer,
+    initialMarkers,
+    mapReducerInitializer
+  )
+  // マーカーの追加・削除のaction関数
+  const addMarker = useCallback(
+    (position) => dispatch({ type: "ADD", payload: position }),
+    [dispatch]
+  )
+  const removeMarker = useCallback(
+    (removeUuid) => dispatch({ type: "REMOVE", payload: removeUuid }),
+    [dispatch]
+  )
+
+  // 外向けにはobjectではなくarrayとして返す
+  const getMarkers = useCallback(
+    () => Object.entries(markers).map(([id, position]) => ({ id, position })),
+    [markers]
+  )
+  return {
+    // markers // objectとしてのmarkerは隠蔽する
+    addMarker,
+    removeMarker,
+    getMarkers
+  }
+}
+
+// Mapがクリックされたらイベントを飛ばすhooks
+export const useMapClickEvent = ({ onClickMap, googleMap, map }) => {
+  useEffect(() => {
+    const listener = googleMap.maps.event.addListener(map, "click", (e) => {
+      onClickMap({
+        lat: e.latLng.lat(),
+        lng: e.latLng.lng()
+      })
+    })
+    // onClickMapが変更されたらつくったイベントをクリアする
+    //（じゃないとクリックするたびにイベントが大量閣下さえる）
+    return () => {
+      googleMap.maps.event.removeListener(listener)
+    }
+  }, [googleMap, map, onClickMap])
+}
---
+++ google-map/src/step5-4/index.tsx
@@ -0,0 +1,131 @@
+import React, { useRef } from "react"
+import {
+  useGoogleMap,
+  useMap,
+  useDrawMapMarker,
+  useMarkerState,
+  useMapClickEvent,
+  useMapInfoWindow,
+  useMarkerClickEvent
+} from "./hooks"
+import styled from "styled-components"
+
+const API_KEY = undefined
+
+const initialConfig = {
+  zoom: 12,
+  center: { lat: 35.6432027, lng: 139.6729435 }
+}
+
+const initialMarkers = [
+  { lat: 35.6432027, lng: 139.6729435 },
+  { lat: 35.5279833, lng: 139.6989209 },
+  { lat: 35.6563623, lng: 139.7215211 },
+  { lat: 35.6167531, lng: 139.5469376 },
+  { lat: 35.6950961, lng: 139.5037899 }
+]
+
+const MapContainer = styled.div`
+  height: 100vh;
+  width: 100%;
+`
+
+const Cloak = styled.div`
+  display: none;
+`
+
+const MarkerInfoWindow = ({ googleMap, map, position, marker }) => {
+  const windowRef = useRef(null)
+
+  const infoWindow = useMapInfoWindow({
+    googleMap,
+    map,
+    marker,
+    contentNode: windowRef.current
+  })
+  useMarkerClickEvent(marker, () => {
+    console.log(windowRef)
+    console.log(infoWindow)
+    console.log("a")
+  })
+  return (
+    <Cloak>
+      <div ref={windowRef}>
+        hello, {position.lat}, {position.lng}
+      </div>
+    </Cloak>
+  )
+}
+
+const MarkerWithWindow = ({ googleMap, map, position }) => {
+  const marker = useDrawMapMarker({
+    googleMap,
+    map,
+    position
+  })
+
+  return (
+    <MarkerInfoWindow
+      googleMap={googleMap}
+      map={map}
+      marker={marker}
+      position={position}
+    />
+  )
+}
+
+const useMapMarkerSetup = ({ googleMap, map }) => {
+  const { addMarker, removeMarker, getMarkers } = useMarkerState(initialMarkers)
+  const markers = getMarkers()
+  // クリックイベントを追加
+  useMapClickEvent({
+    onClickMap: ({ lat, lng }) => {
+      addMarker({ lat, lng })
+    },
+    map,
+    googleMap
+  })
+  return { markers, removeMarker }
+}
+
+const MapMarkers: React.SFC<any> = ({ map, googleMap }) => {
+  const { markers } = useMapMarkerSetup({ map, googleMap })
+  return (
+    <>
+      {markers.map(({ id, position }) => (
+        <MarkerWithWindow
+          key={id} // hooksがkeyに紐づく。これがないと適切なマーカーが消えなくなる
+          position={position}
+          map={map}
+          onClickMarker={() => {}}
+          googleMap={googleMap}
+        />
+      ))}
+    </>
+  )
+}
+
+const WaitForMap = ({ googleMap, map, children }) => {
+  if (!googleMap || !map) {
+    return null
+  }
+  return children
+}
+
+export const MapApp = () => {
+  const googleMap = useGoogleMap(API_KEY)
+  const mapContainerRef = useRef(null)
+  const map = useMap({
+    googleMap,
+    mapContainerRef,
+    initialConfig
+  })
+  return (
+    <>
+      <MapContainer ref={mapContainerRef} />
+      <WaitForMap googleMap={googleMap} map={map}>
+        <MapMarkers googleMap={googleMap} map={map} />
+      </WaitForMap>
+    </>
+  )
+}
